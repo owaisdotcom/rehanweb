0ef60a02eb97c45746d2a0e1e6821bd142f59b30
Owais <owais.ansari@octdaily.com>
2025-01-17 04:41:20 +0500

    push

--- src/components/layout/header/DropdownDashboard.js
+++ src/components/layout/header/DropdownDashboard.js
@@ -11,6 +11,7 @@ const DropdownDashboard = () => {
   useEffect(() => {
     const fetchCategories = async () => {
       try {
+        
         const response = await fetch("https://mathsflix-backend.vercel.app/api/products/categories");
         if (!response.ok) {
           throw new Error("Failed to fetch categories");

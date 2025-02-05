6eae4bf3a85ac881ff77680c252a75000c1bf4b9
Owais <owais.ansari@octdaily.com>
2025-02-05 23:17:10 +0500

    push

--- src/components/sections/contact-form/ContactFrom.js
+++ src/components/sections/contact-form/ContactFrom.js
@@ -44,7 +44,7 @@ const ContactFrom = () => {
     setResponseMessage("");
 
     try {
-      const response = await fetch("http://localhost:4000/submit-form", {
+      const response = await fetch("https://mathsflix-backend.vercel.app/submit-form", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
